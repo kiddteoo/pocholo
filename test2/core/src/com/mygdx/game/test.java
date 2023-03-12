7ccf26822df52a251d2d4529131ef80a61296c9c
meriendaa <a19teomerrod@inspedralbes.cat>
2023-03-12 21:48:38 +0100

    ok

---
+++ test2/core/src/com/mygdx/game/test.java
@@ -0,0 +1,70 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class test extends ApplicationAdapter {
+	Texture img;
+	private Texture bucketImage;
+	private Music rainMusic;
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+	private Rectangle bucket;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+		rainMusic.setLooping(true);
+		rainMusic.play();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64 / 2; // center the bucket horizontally
+		bucket.y = 20; // bottom left corner of the bucket is 20 pixels above the bottom screen edge
+		bucket.width = 64;
+		bucket.height = 64;
+
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+
+		// tell the camera to update its matrices.
+		camera.update();
+
+		// tell the SpriteBatch to render in the
+		// coordinate system specified by the camera.
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(bucketImage, bucket.x, bucket.y);
+		batch.end();
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)) bucket.y += 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) bucket.y -= 200 * Gdx.graphics.getDeltaTime();
+
+		// make sure the bucket stays within the screen bounds
+		if(bucket.x < 0) bucket.x = 0;
+		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
+	}
+	
+	@Override
+	public void dispose () {
+		bucketImage.dispose();
+		rainMusic.dispose();
+		batch.dispose();
+
+	}
+}
